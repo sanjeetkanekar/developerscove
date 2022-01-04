a0a8e629ef2cfb37187b85820e8bad1604d3308a
sanjeetkanekar <sanjeet.kanekar@yahoo.in>
2022-01-04 21:02:23 +0530

    made content changes

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -158,3 +158,34 @@ $('.filterable-btn').on( 'click', 'button', function() {
   $grid.isotope({ filter: filterValue });
 });
 
+$(document).ready(function () {
+
+  $(document).ready(function () {
+
+		$("#sticker").sticky({ topSpacing: 0 });
+	});
+
+	var scrollLink = $('.smoothScroll');
+
+	// Smooth scrolling
+	scrollLink.click(function (e) {
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 1000);
+	});
+
+  $(window).scroll(function () {
+		var scrollbarLocation = $(this).scrollTop();
+
+		scrollLink.each(function () {
+
+			var sectionOffset = $(this.hash).offset().top - 20;
+
+			if (sectionOffset <= scrollbarLocation) {
+				$(this).parent().addClass('active');
+				$(this).parent().siblings().removeClass('active');
+			}
+		});
+	});
+});
